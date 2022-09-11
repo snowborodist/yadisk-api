000e9d94519e8eea0d41001d738b2a9c1e34990f
snowborodist <snowborodist@gmail.com>
2022-09-11 17:21:21 +0300

    change upsert to pg_on_conflict_do_nothing

--- src/yadisk_api/db/repositories/SystemItemRepository.py
+++ src/yadisk_api/db/repositories/SystemItemRepository.py
@@ -16,17 +16,20 @@ class SystemItemRepository(BaseRepository):
         for _ in results:
             raise ValueError("Imported items contain parent links to items of type 'FILE'")
 
-    async def get_existing_item_ids(self, item_ids: list[str]) -> set[str]:
-        # noinspection PyUnresolvedReferences
-        results = await self.session.execute(
-            select(SystemItem.id).filter(SystemItem.id.in_(item_ids))
-        )
-        return set([item_id for item_id, *_ in results])
+    # async def get_existing_item_ids(self, item_ids: list[str]) -> set[str]:
+    #     # noinspection PyUnresolvedReferences
+    #     results = await self.session.execute(
+    #         select(SystemItem.id).filter(SystemItem.id.in_(item_ids))
+    #     )
+    #     return set([item_id for item_id, *_ in results])
 
-    async def emplace_items(self, items: list[SystemItem]):
-        self.session.add_all(items)
+    async def upsert_items(self, items: list[SystemItem]):
+        values = [{"id": item.id, "type": item.type} for item in items]
+        stmt = pg_insert(SystemItem).values(values).on_conflict_do_nothing()
+        _ = await self.session.execute(stmt)
+        # self.session.add_all(items)
 
-    async def emplace_item_updates(self, item_updates: list[SystemItemUpdate]):
+    async def insert_item_updates(self, item_updates: list[SystemItemUpdate]):
         self.session.add_all(item_updates)
 
     async def get_item(self, system_item_id: str) -> SystemItem | None:
